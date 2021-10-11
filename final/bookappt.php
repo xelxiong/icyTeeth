4494c1ded2f0260c70c09899e07b6b39d94fa56e
Xeleceem Xiong <81644979+xelxiong@users.noreply.github.com>
2021-10-11 14:01:56 -0500

    5/2/2021

---
+++ final/bookappt.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta name="description" content="Xeleceem's contact form">
+    <meta name="keywords" content="Xeleceem's Contact Form">
+	<title>Xeleceem's Contact Form</title>
+    <link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="stylesheet" href="styles/contact.css">
+</head>
+
+<body>
+	    <h1>Email Confirmation</h1>
+		<fieldset>
+		<fieldset>
+    		<legend>Appointment Information</legend>
+			<label for="appointment_date">Today's  Date:</label>
+			<input type="date" name="appointment_date" id="appointment_date" value="<?php echo $_REQUEST['appointment_date'] ?>" disabled><br>
+			<label for="subject">Subject:</label>
+			<input type="text" name="subject" id="subject" value="<?php echo $_REQUEST['subject'] ?>" disabled><br>
+			<label for="Message">Notes:</label>
+			<textarea id="message" name="message" rows="4" disabled><?php echo $_REQUEST['message'] ?></textarea><br>
+    		<label for="first_name">First Name:</label>
+			<input type="text" name="first_name" id="first_name" value="<?php echo $_REQUEST['first_name'] ?>" disabled><br>
+			<label for="last_name">Last Name:</label>
+			<input type="text" name="last_name" id="last_name" value="<?php echo $_REQUEST['last_name'] ?>" disabled><br>
+        	<label for="email">Email Address:</label>
+        	<input type="email" name="email" id="email" value="<?php echo $_REQUEST['email'] ?>" disabled><br>
+			<label for="phone">Phone Number:</label>
+			<input type="tel" name="phone" id="phone" value="<?php echo $_REQUEST['phone'] ?>" disabled><br>
+		</fieldset>
+        </fieldset>
+<!-- This entire script, including the opening and closing ?php tags, can be nested inside of any other tag, such as div or p, to control positioning and formatting of confirmation message spit out by the email script -->
+<h2>
+<?php
+  if (isset($_REQUEST['email'])) { //if "email" variable is filled out, send email
+  
+  //Set admin email for email to be sent to (use you own MATC email address)
+    $admin_email = "xiongxc@gmatc.matc.edu"; 
+
+  //Set PHP variable equal to information completed on the HTML form
+    $email = $_REQUEST['email']; //Request email that user typed on HTML form
+    $phone = $_REQUEST['phone']; //Request phone that user typed on HTML form
+    $reservation_date = $_REQUEST['appointment_date']; //Request subject that user typed on HTML form
+    $subject = $_REQUEST['subject']; //Request subject that user typed on HTML form
+    $message = $_REQUEST['message']; //Request message that user typed on HTML form
+  //Combine first name and last name, adding a space in between
+    $name = $_REQUEST['first_name'] . " " .  $_REQUEST['last_name']; 
+            
+  //Start building the email body combining multiple values from HTML form    
+    $body  = "From: " . $name . "\n"; 
+    $body .= "Email: " . $email . "\n"; //Continue the email body
+    $body .= "Phone: " . $phone . "\n"; //Continue the email body
+    $body .= "Reservation Date: " . $reservation_date . "\n"; //Continue the email body
+    $body .= "Message: " . $message; //Continue the email body
+  
+  //Create the email headers for the from and CC fields of the email     
+    $headers = "From: " . $name . " <" . $email . "> \r\n"; //Create email "from"
+    $headers .= "CC: " . $name . " <" . $email . ">"; //Send CC to visitor
+    
+  //Actually send the email from the web server using the PHP mail function
+  mail($admin_email, $subject, $body, $headers); 
+    
+  //Display email confirmation response on the screen
+  echo "Thank you for scheduling an appointment!"; 
+  }
+  
+  //if "email" variable is not filled out, display an error
+  else  { 
+     echo "There has been an error!";
+        }
+?>
+
+</h2>
+</body>
+</html>
